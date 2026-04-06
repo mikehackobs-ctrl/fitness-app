6d5aa391e1830d222664edc257127e65572a293c
mikehackobs-ctrl <mikehackobs@gmail.com>
2026-04-06 22:38:23 +0400

    Create main application file for Fitness App

---
+++ mobile/App.js
@@ -0,0 +1,9 @@
+// Main Application file for the Fitness App
+import React from 'react';
+import { Navigation } from './navigation';
+
+const App = () => {
+  return <Navigation />;
+};
+
+export default App;
